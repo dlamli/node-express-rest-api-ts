d61ea13533e71b47bb4f2b59b00861beb75df93a
dlamli <dannylamli@hotmail.com>
2025-05-02 16:58:25 -0600

    feat:Add authentication

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,6 +1,34 @@
-import { createUser, getUserByEmail } from "db/models/users.js";
 import { Request, Response } from "express";
+import { createUser, getUserByEmail } from "db/models/users.js";
 import { random, authentication } from "helpers/index.js";
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) return res.sendStatus(400);
+
+    const user = await getUserByEmail(email).select("+authentication.salt +authentication.password");
+
+    if (!user) return res.sendStatus(400);
+
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password !== expectedHash.toString()) return res.sendStatus(403)
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(salt, user._id.toString()).toString();
+
+    await user.save();
+
+    res.cookie('AUTH-LOGIN', user.authentication.sessionToken, { domain: 'localhost', path: '/' })
+
+    return res.status(200).json(user).end()
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
 
 export const register = async (req: Request, res: Response) => {
   try {
--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,6 +1,7 @@
-import { register } from "controllers/authentication.js";
-import express, { Router } from "express";
+import { login, register } from "controllers/authentication.js";
+import { Router } from "express";
 
-export default (router: express.Router) => {
+export default (router: Router) => {
   router.post('/auth/register', register);
+  router.post('/auth/login', login)
 }
